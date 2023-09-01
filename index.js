30f660a738b70b1a8bf9ead1d9600183f0bcddf6
Manuel <damanuel2007@gmail.com>
2023-09-01 18:36:42 -0300

    added frontend production build

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
-
+app.use(express.static('dist'))
 let persons = [
     { 
       "id": 1,
