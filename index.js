e49efb565e90fc28e16688bc03b61862a21fba9a
Manuel <damanuel2007@gmail.com>
2023-09-09 17:09:23 -0300

    uibuild

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     if(error.name === 'CastError'){
         return response.status(400).send({error: 'malformatted id'})
+    }else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
     }
     next(error)
 }
@@ -44,14 +46,8 @@ app.delete('/api/persons/:id', (request, response) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-    
-    if(!body.name || !body.number){
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
     const person = new Person({
         name: body.name,
         number: body.number,
@@ -59,6 +55,7 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 
 })
 app.use(errorHandler)
--- models/person.js
+++ models/person.js
@@ -13,10 +13,17 @@ mongoose.connect(uri)
     .catch((error) => {
         console.log('error connecting to MongoDB:', error.message)
     })
-
+    
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type:String,
+        minLength: 3,
+        required: true,
+    },
+    number: {
+        type: String,
+        required: true,
+    },
 })
 
 personSchema.set('toJSON', {
